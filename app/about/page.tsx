29fe5ccce1ea8f2b8cc93014961e834fd3b27c36
Adam Wehunt <adamwehunt@Marisas-MacBook-Pro.local>
2023-04-04 16:12:35 -0700

    Css vars.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,7 @@
 import styles from './about.module.css';
 
 export default function About() {
-	return <main className={styles.main}></main>;
+	return <main className={styles.main}>
+		
+	</main>;
 }
